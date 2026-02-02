d666d2a72ead62b0454f58044ed0cad3247fda76
KobeGaigoKyoumu <gaigokyoumu@gmail.com>
2026-02-03 00:27:55 +0900

    chore: update metadata with mobile icons

---
+++ src/app/manifest.js
@@ -0,0 +1,18 @@
+export default function manifest() {
+    return {
+        name: 'LMS 神戸外語',
+        short_name: 'LMS',
+        description: '神戸外語 LMS Application',
+        start_url: '/',
+        display: 'standalone',
+        background_color: '#ffffff',
+        theme_color: '#3b82f6',
+        icons: [
+            {
+                src: '/icon.png',
+                sizes: 'any',
+                type: 'image/png',
+            },
+        ],
+    }
+}
